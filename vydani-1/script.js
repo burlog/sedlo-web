9354d51937abb8054a849e3c3b8056410d194e0c
Michal Bukovský <burlog@seznam.cz>
2025-06-30 07:55:04 +0200

    Update

--- vydani-1/script.js
+++
@@ -1,9 +0,0 @@
-function fixFirefoxGap() {
-    if (navigator.userAgent.toLowerCase().includes("firefox")) {
-        document.querySelectorAll("[data-gap]").forEach(el => {
-            const gap = el.getAttribute("data-gap");
-            if (gap)
-                el.style.marginTop = gap;
-        });
-    }
-}
